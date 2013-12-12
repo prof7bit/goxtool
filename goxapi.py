9000a4983edfcf3c3953cd9d8ca448e2a1af5017
prof7bit <prof7bit@googlemail.com>
2013-12-12 12:45:44 +0100

    return False to pubnub when terminating

--- goxapi.py
+++ goxapi.py
@@ -1338,7 +1338,7 @@ class PubnubClient(BaseClient):
         """callback method called by pubnub when a message is received"""
         self.signal_recv(self, msg)
         self._time_last_received = time.time()
-        return True
+        return not self._terminating
 
     def channel_subscribe(self, download_market_data=False):
         # no channels to subscribe, this happened on connect already
