0efff43159ac74536226fc5441f3afc0da1b3e74
prof7bit <prof7bit@googlemail.com>
2013-12-13 19:04:19 +0100

    update comments

--- pubnub_light.py
+++ pubnub_light.py
@@ -1,8 +1,22 @@
-"""
-light pubnub API, avoiding unnecessary
-reconnects, only subscribing is implemented.
-(c) 2013 Bernd Kreuss
-"""
+"""pubnub light API (only subscribe, not publish)"""
+
+#  Copyright (c) 2013 Bernd Kreuss <prof7bit@gmail.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+
 import base64
 from Crypto.Cipher import AES
 import hashlib
